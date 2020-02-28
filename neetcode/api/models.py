666421ccae3f93d03c0fb5555a4085e0a44244c8
Dong <56413707+epeenphrine@users.noreply.github.com>
2020-02-28 10:18:11 -0500

    allowed hosts

--- neetcode/api/models.py
+++ neetcode/api/models.py
@@ -84,3 +84,28 @@ class About(models.Model):
         verbose_name_plural = 'About Role'
     def __str__(self):
         return self.role 
+
+class Language(models.Model):
+    language = models.CharField(max_length=200)
+    slug = models.CharField(max_length=200)
+
+class WebApp(models.Model):
+    webapp = models.CharField(max_length=200)
+    description = models.TextField('brief description')
+    languages = models.CharField(max_length=200)
+    libraries = models.TextField('Libraries used')
+    language_slug = models.CharField(max_length=200, default=1)
+    slug = models.CharField(max_length=200, default=1)
+
+##
+class Project(models.Model):
+    
+    project = models.CharField(max_length=200)
+    description = models.TextField('brief description')
+    libraries = models.TextField('Libraries used:')
+    language = models.CharField(max_length=200, default=1)
+    language_slug = models.CharField(max_length=200, default=1)
+    slug = models.CharField(max_length=200, default=1)
+
+    def __str__(self):
+        return self.project
--- neetcode/neetcode/settings.py
+++ neetcode/neetcode/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '01no09g!71%t#n8ep)6wg^_h%^dptb=*6q1pvibg0r$wiq$em$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
